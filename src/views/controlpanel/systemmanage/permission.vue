625970d78f48385e0359ce5f0f2700dfc8abb6db
stevendi <dhh@my.swjtu.edu.cn>
2019-12-02 00:14:32 +0800

    修改页面内容

--- src/views/controlpanel/systemmanage/permission.vue
+++ src/views/controlpanel/systemmanage/permission.vue
@@ -1,13 +1,56 @@
 <template>
-    <span>permission</span>
+<!--  变量名待修改-->
+  <div class="app-container">
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="拼命加载中" border fit
+              highlight-current-row>
+      <el-table-column align="center" label="序号" width="80">
+        <template slot-scope="scope">
+          <span v-text="getIndex(scope.$index)"> </span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="分类" prop="icon" width="150"></el-table-column>
+      <el-table-column align="center" label="名称" prop="permissionName"></el-table-column>
+      <el-table-column align="center" label="说明" prop="description" width="420"></el-table-column>
+      <el-table-column align="center" label="角色">
+        <template slot-scope="scope">
+          <div v-for="role in scope.row.roles">
+            <div v-text="role.roleName" style="display: inline-block;vertical-align: middle;"></div>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: "permission"
+  export default {
+    data() {
+      return {
+        list: [],//表格的数据
+        listLoading: false,//数据加载等待动画
+      }
+    },
+    created() {
+      this.getAllPermisson();
+    },
+    methods: {
+      getAllPermisson() {
+        //查询所有权限
+        this.api({
+          url: "/user/selectAllPermission",
+          method: "get"
+        }).then(data => {
+          this.list = data.list;
+        })
+      },
+      getIndex($index) {
+        //表格序号
+        return $index + 1
+      },
     }
+  }
 </script>
-
 <style scoped>
-
+  .requiredPerm {
+    color: #ff0e13;
+  }
 </style>
--- src/views/controlpanel/webmanage/news.vue
+++ src/views/controlpanel/webmanage/news.vue
@@ -1,13 +1,167 @@
 <template>
-    <span>news</span>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-form>
+        <el-form-item>
+          <el-button type="primary" icon="plus" @click="showCreate" v-if="hasPerm('news:add')">添加
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="拼命加载中" border fit
+              highlight-current-row>
+      <el-table-column align="center" label="序号" width="80">
+        <template slot-scope="scope">
+          <span v-text="getIndex(scope.$index)"> </span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="newsTitle" label="题目" style="width: 60px;"/>
+      <el-table-column align="center" label="类型" style="width: 60px;">
+        <template slot-scope="scope">
+          <span v-text="getType(scope.row.newsType)"/>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="创建时间" width="170">
+        <template slot-scope="scope">
+          <span>{{scope.row.newsTime}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="管理" width="200" v-if="hasPerm('news:update')">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="edit" @click="showUpdate(scope.$index)">修改</el-button>
+          <el-button type="primary" icon="edit" @click="showUpdate(scope.$index)" v-if="hasPerm('news:delete')">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="listQuery.pageNum"
+      :page-size="listQuery.pageRow"
+      :total="totalCount"
+      :page-sizes="[10, 20, 50, 100]"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form class="small-space" :model="tempArticle" label-position="left" label-width="60px"
+               style='width: 300px; margin-left:50px;'>
+        <el-form-item label="文章">
+          <el-input type="text" v-model="tempArticle.content">
+          </el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button v-if="dialogStatus=='create'" type="success" @click="createArticle">创 建</el-button>
+        <el-button type="primary" v-else @click="updateArticle">修 改</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: "news"
+  export default {
+    data() {
+      return {
+        totalCount: 0, //分页组件--数据总条数
+        list: [],//表格的数据
+        listLoading: false,//数据加载等待动画
+        listQuery: {
+          pageNum: 1,//页码
+          pageRow: 50,//每页条数
+          name: ''
+        },
+        dialogStatus: 'create',
+        dialogFormVisible: false,
+        textMap: {
+          update: '编辑',
+          create: '创建文章'
+        },
+        tempArticle: {
+          id: "",
+          content: ""
+        }
+      }
+    },
+    created() {
+      this.getList();
+    },
+    methods: {
+      getList() {
+        //查询列表
+        if (!this.hasPerm('article:list')) {
+          return
+        }
+        this.listLoading = true;
+        this.api({
+          url: "/article/listArticle",
+          method: "get",
+          params: this.listQuery
+        }).then(data => {
+          this.listLoading = false;
+          this.list = data.list;
+          this.totalCount = data.totalCount;
+        })
+      },
+      handleSizeChange(val) {
+        //改变每页数量
+        this.listQuery.pageRow = val
+        this.handleFilter();
+      },
+      handleCurrentChange(val) {
+        //改变页码
+        this.listQuery.pageNum = val
+        this.getList();
+      },
+      getIndex($index) {
+        //表格序号
+        return (this.listQuery.pageNum - 1) * this.listQuery.pageRow + $index + 1
+      },
+      showCreate() {
+        //显示新增对话框
+        this.tempArticle.content = "";
+        this.dialogStatus = "create"
+        this.dialogFormVisible = true
+      },
+      showUpdate($index) {
+        //显示修改对话框
+        this.tempArticle.id = this.list[$index].id;
+        this.tempArticle.content = this.list[$index].content;
+        this.dialogStatus = "update"
+        this.dialogFormVisible = true
+      },
+      createArticle() {
+        //保存新文章
+        this.api({
+          url: "/article/addArticle",
+          method: "post",
+          data: this.tempArticle
+        }).then(() => {
+          this.getList();
+          this.dialogFormVisible = false
+        })
+      },
+      updateArticle() {
+        //修改文章
+        this.api({
+          url: "/article/updateArticle",
+          method: "post",
+          data: this.tempArticle
+        }).then(() => {
+          this.getList();
+          this.dialogFormVisible = false
+        })
+      },
+      getType(val){
+        if (val===1){
+          return "公司新闻";
+        }
+        if (val===2){
+          return "行业新闻";
+        }
+        if (val===3){
+          return "通知公告";
+        }
+      }
     }
+  }
 </script>
-
-<style scoped>
-
-</style>
